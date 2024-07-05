91e09153c704efb3d53de330f59ceab949adf06b
wujiabao <wujiabao@leateckgroup.com>
2024-07-05 18:06:50 +0800

    -

--- src/parseHTML.js
+++ src/parseHTML.js
@@ -2,22 +2,76 @@ import {
     startTagOpen,
     startTagClose,
     dynamicArgAttribute,
-    attribute
+    attribute,
+    endTag
 } from "./reg";
 
 /**
  * 解析模板
  */
 export function parseHTML(html){
+    /**
+     * 主要用于追踪和管理模板中的嵌套标签结构。
+     * 当解析器遇到一个开始标签（例如 <div> 或 <p>）时，它会创建一个对象来描述这个标签的信息，如标签名、属性、开始和结束位置等，并将这个对象压入 stack 数组中。
+     * 
+     * 1. 标签压入：当解析器遇到一个开始标签时，它会将这个标签的信息作为一个对象放入 stack。这表示当前标签的开始，解析器现在处于这个标签的内部。
+     * 2. 标签层级管理：stack 的顶部始终是当前正在处理的最内层的标签。每当遇到一个新的开始标签，它都会被压入 stack 的顶部，形成标签的嵌套关系。
+     * 3. 标签弹出: 当解析器遇到一个结束标签（例如 </div> 或 </p>）时，它会检查 stack 的顶部对象，看是否与当前结束标签相匹配。如果匹配，
+     *      表示当前标签的结束，解析器会从 stack 中移除（弹出）最顶层的对象，恢复到上一层级的标签环境。
+     * 4. 错误检测: 如果 stack 为空时遇到结束标签，或者结束标签与 stack 顶部的开始标签不匹配，这通常表示模板中的标签没有正确闭合，解析器会抛出错误。
+     * 5. 模板结构还原: 在整个解析过程中，stack 的状态反映了模板的结构。解析结束后，stack 应该为空，表示所有开始标签都被正确地闭合。
+     */
+    const stack = [];
     // 用于追踪上一次循环处理后剩余未解析的 HTML 字符串部分
     let last;
     while(html){
         last = html;
 
+        /**
+         * textEnd 变量用于标识HTML字符串中下一个 < 字符的位置。
+         * 这个变量帮助确定了普通文本内容（非HTML标签）的结束位置。
+         * 
+         * 具体来说，textEnd 是通过调用 html.indexOf('<') 获得的，这会在给定的HTML字符串中查找第一个 < 字符的索引位置。
+         * 如果 < 字符在字符串中存在，indexOf 方法会返回该字符的索引；如果不存在，则返回 -1。
+         * 
+         * 在模板解析的过程中，textEnd 的作用如下：
+         * 
+         * 1. 文本内容的界定：当 Vue 解析器在处理文本内容时，它需要知道文本何时结束以及何处开始解析HTML标签。
+         *      textEnd 提供了这一信息，允许解析器区分文本内容和HTML标签。
+         * 2. 性能优化：通过只扫描到下一个 < 字符的位置，解析器可以避免不必要的全字符串搜索，提高解析效率。
+         *      一旦找到 < 字符，解析器就可以停止当前的文本读取，转而开始解析新的HTML标签。
+         * 3. 错误处理：如果在某个点 html 字符串为空或 textEnd 等于 -1，这意味着字符串中不再有 < 字符，
+         *      可能指示模板中存在未闭合的标签或格式错误。
+         */
         let textEnd = html.indexOf('<');
         if(textEnd === 0){
-            parseStartTag();
-            break;
+
+            const endTagMatch = html.match(endTag);
+            if(endTagMatch){
+                advance(endTagMatch[0].length);
+                parseEndTag(endTagMatch[1], curIndex, index)
+                continue;
+            }
+
+            // 解析开始标签
+            const startTagMatch = parseStartTag();
+            if(startTagMatch){
+                handleStartTag(startTagMatch);
+                continue;
+            }
+        }
+        let rest,text
+        if(textEnd >= 0){
+            rest = html.slice(textEnd)
+            text = html.substring(0, textEnd)
+        }
+
+        if (textEnd < 0) {
+            text = html
+        }
+
+        if (text) {
+            advance(text.length)
         }
     }
 
@@ -30,6 +84,8 @@ export function parseHTML(html){
      * 在解析器识别并处理完 <html> 和 <body> 标签后，假设总共处理了12个字符，那么调用 advance(12) 之后，html 的值就会变成
      * <p>Hello World!</p></body></html>
      * 现在解析器可以继续从 <p> 标签开始解析剩下的字符串。这就是 advance 函数的主要用途和意义。
+     * 
+     * 通过维护 stack，Vue 的解析器能够构建一个关于模板结构的抽象语法树（Abstract Syntax Tree，AST），这个 AST 后续会被用于生成对应的 VNode（虚拟节点），并最终渲染成真实的 DOM。AST 的构建过程依赖于 stack 来记录和管理标签的层级关系，确保每个开始标签都有对应的结束标签，从而保证模板的正确性和完整性。
      */
     function advance(n){
         html = html.substring(n);
@@ -37,26 +93,61 @@ export function parseHTML(html){
 
     function parseStartTag(){
         const start = html.match(startTagOpen);
-        console.log("parseStartTag==>", start);
         if(start){
             const match = {
                 tagName:start[1],
                 attrs:[]
             }
             advance(start[0].length)
-            let end,attr
+            let end,attr 
 
-            attr = html.match(attribute) 
-            
             while(
-                // 直到找到结束标签否则一直循环
+                // 匹配不到开始标签对应的闭合标签 > 
                 !(end = html.match(startTagClose))
-                && (attr = html.match(dynamicArgAttribute))
+                // 匹配到属性 或者动态属性
+                && (attr = html.match(dynamicArgAttribute) || html.match(attribute))
             ){
-                console.log(end,attr)
+                advance(attr[0].length)
+                match.attrs.push(attr);
             }
-            
-            
+            if(end){
+                advance(end[0].length)
+                return match;
+            }
         }
+    }
+
+    function parseEndTag(tagName,start){
+        let lowerCasedTagName;
+        if(tagName){
+            lowerCasedTagName = tagName.toLowerCase();
+        }
+    }
+
+    function handleStartTag(match){
+        const tagName = match.tagName;
+        // 获取属性的数量
+        const l = match.attrs.length
+        // 初始化一个数组来存储标签的属性
+        const attrs = new Array(l)
+        for(let i = 0; i < l; i++){
+            const args = match.attrs[i];
+            /**
+             * 1.不带引号的简单属性值：<div class=abc>
+             * 2.带单引号的属性值：<div class='abc'>
+             * 3.带双引号的属性值：<div class="abc">
+             * 4.动态属性值：<div :class="someVariable">
+             */
+            const value = args[3] || args[4] || args[5] || ''
+            attrs[i] = {
+                name: args[1],
+                value: value
+            }
+        }
+        stack.push({
+            tag: tagName,
+            lowerCasedTag: tagName.toLowerCase(),
+            attrs
+        });
     }
 }
